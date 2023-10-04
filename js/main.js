089cb406fcfc432ba0f66c2ee31368f8896d6675
juliaslares <juliabelensl01@gmail.com>
2023-10-03 23:31:07 -0300

    js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,3 @@ let saludo = "buen día"
 alert ("Bienvenidx a Arte-Geek")
 
 
-const persona1 = {
-    nombre : "A",
-    edad: 39,
-    calle: "Av. Siempreviva 742",
-    saludar: function () {
-        return  `Hola, me llamo ${this.nombre}!`; 
-    },
- };
-
-
