a20e30ab9cd1775a2944288d0199522d85790447
juliaslares <juliabelensl01@gmail.com>
2023-10-03 23:31:00 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 let saludo = "buen día"
 alert ("Bienvenidx a Arte-Geek")
+
+
+const persona1 = {
+    nombre : "A",
+    edad: 39,
+    calle: "Av. Siempreviva 742",
+    saludar: function () {
+        return  `Hola, me llamo ${this.nombre}!`; 
+    },
+ };
+
+
