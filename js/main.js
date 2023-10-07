b288edc31a5824c80b9d61d6e384b8d50f93c95e
SofiaSalerno1 <sofiasalernofront@gmail.com>
2023-10-07 17:10:33 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ if(nombre == ''){
     alert('No ingresaste tu nombre')
 }
 
-alert ("Bienvenidx a Arte-Geek, " + nombre + ". Ayudanos a personalizar la página para vos<3")
+alert ("Bienvenidx a Arte-Geek, " + nombre + ".\nAyudanos a personalizar la página para vos<3")
+
 
 let lugar = prompt("Ingresa el país donde vivís");
 let color = prompt("Cuél es tu color preferido?");
