0d8a54717aa2dbe64a506cdc794f97492d19c973
juliaslares <141635740+juliaslares@users.noreply.github.com>
2023-10-18 10:10:52 -0300

    Rama3 (#11)
    * index
    
    * no dupl
    
    * no+dupl
    
    * m,
    
    * monts
    
    * menu
    
    * css
    
    * css2
    
    * css3
    
    * flex
    
    * js
    
    * css
    
    * js
    
    * color
    
    * css
    
    * css
    
    * js
    
    * js
    
    * .s
    
    * intento de formulario

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 let nombre = prompt("Ingresa tu nombre");
 if(nombre == ''){
     alert('No ingresaste tu nombre')
@@ -10,4 +11,25 @@ let lugar = prompt("Ingresa el país donde vivís");
 let color = prompt("Cuél es tu color preferido?");
 
 
+
+let formulario= document.getElementById('formulario.php');
+
+document.getElementById("nombreyapellido").value = "";
+document.getElementById("correo").value = "";
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+   let inputNombre = document.querySelector('nombreyapellido'); 
+   if (inputNombre.value == "" ){
+    alert('El campo nombre tiene que estar completo')
+
+   }else if( inputNombre.value.length < 3){
+    alert('El campo nombre debe tener al menos 3 caracteres')
+   }
+   });
+   
+
+const form = document.getElementById('formulario.php');
+let Nombre = form.elements['nombreyapellido'];
+let email = form.elements['correo'];
 
